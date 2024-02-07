abe58334609eeefd873ef260d3b89882d974e4f4
מיטל <ronni.ban@sisma.org.il>
2024-02-07 19:33:22 +0200

    commit 6

--- app/src/main/java/com/example/ronibananoedittext/MainActivity.java
+++ app/src/main/java/com/example/ronibananoedittext/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.ronibananoedittext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -19,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText et1;
     private EditText et2;
     private EditText et3;
+    private ImageView iv1;
+    private ImageView iv2;
+    private ImageView iv3;
+
     private Button check1;
     private Button check2;
     private Button check3;
@@ -30,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private int rnd4;
     private int rnd5;
     private int rnd6;
+    int sum = 0;
 
 
 
@@ -49,6 +57,9 @@ public class MainActivity extends AppCompatActivity {
         et1 = findViewById(R.id.editText1);
         et2 = findViewById(R.id.editText2);
         et3 = findViewById(R.id.editText3);
+        iv1 = findViewById(R.id.imageView1);
+        iv2 = findViewById(R.id.imageView2);
+        iv3 = findViewById(R.id.imageView3);
         check1 =findViewById(R.id.button1);
         check2 = findViewById(R.id.button2);
         check3 = findViewById(R.id.button3);
@@ -57,12 +68,77 @@ public class MainActivity extends AppCompatActivity {
         rnd1=random_number();
         rnd2=random_number();
 
-        num1.setText(rnd1);
-        num2.setText(rnd2);
+        num1.setText(""+rnd1);
+        num2.setText(""+rnd2);
+        num3.setText("number");
+        num4.setText("number");
+        num5.setText("number");
+        num6.setText("number");
     }
 
     public static int random_number(){
         Random random=new Random();
         return (int)(Math.random()*89+10);
+    }
+
+    public void check_answer(View view) {
+        String ans1=et1.getText().toString();
+        iv1.setVisibility(view.VISIBLE);
+        if(Integer.parseInt(ans1)!=(rnd1+rnd2))
+            iv1.setImageResource(R.drawable.img_1);
+        else{
+            iv1.setImageResource(R.drawable.img);
+            sum++;
+        }
+        rnd3= rnd1+rnd2;
+        rnd4 = random_number();
+        num3.setText(""+rnd3);
+        num4.setText(""+rnd4);
+
+    }
+
+    public void check_answer2(View view) {
+        String ans2=et2.getText().toString();
+        iv2.setVisibility(view.VISIBLE);
+        if(Integer.parseInt(ans2)!=(rnd3+rnd4))
+            iv2.setImageResource(R.drawable.img_1);
+        else{
+            iv2.setImageResource(R.drawable.img);
+            sum++;
+        }
+        rnd5 = rnd3+rnd4;
+        rnd6 = random_number();
+        num5.setText(""+rnd5);
+        num6.setText(""+rnd6);
+    }
+
+    public void check_answer3(View view) {
+        String ans3=et3.getText().toString();
+        iv3.setVisibility(view.VISIBLE);
+        if(Integer.parseInt(ans3)!=(rnd5+rnd6))
+            iv3.setImageResource(R.drawable.img_1);
+        else{
+            iv3.setImageResource(R.drawable.img);
+            sum++;
+        }
+        double success = ((double)sum/3)*100;
+        Toast.makeText(MainActivity.this,(sum+"/3"+","+success+"%"),Toast.LENGTH_LONG).show();
+        sum=0;
+    }
+    public void restart(View view) {
+        iv1.setVisibility(view.INVISIBLE);
+        iv2.setVisibility(view.INVISIBLE);
+        iv3.setVisibility(view.INVISIBLE);
+        rnd1=random_number();
+        rnd2=random_number();
+        num1.setText(""+rnd1);
+        num2.setText(""+rnd2);
+        num3.setText("number");
+        num4.setText("number");
+        num5.setText("number");
+        num6.setText("number");
+        et1.setText(null);
+        et2.setText(null);
+        et3.setText(null);
     }
 }
